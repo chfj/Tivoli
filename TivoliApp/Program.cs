474fb270b48698dedda33fcf741a63b7eaf77d5b
Christian f. Jørgensen <crille@Christians-MacBook-Pro.local>
2020-03-03 14:23:05 +0100

    Start

---
+++ TivoliApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TivoliApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

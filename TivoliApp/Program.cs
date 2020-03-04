35c15bfb48a39019da045af5e750a3d4900a676f
Christian f. Jørgensen <crille@Christians-MacBook-Pro.local>
2020-03-04 13:12:30 +0100

    I Think I solved all tasks, but i want to see if i can make it more nice

--- TivoliApp/Program.cs
+++ TivoliApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace TivoliApp
 {
@@ -46,6 +47,19 @@ namespace TivoliApp
             Console.WriteLine($"Antal linjer i fil:                     {allLogLines.Count()}");
             Console.WriteLine($"Antal linier der kunne analyseres:       {allLogLines.Where(x => x.IsValid).Count()}");
             Console.WriteLine($"Antal linier der ikke kunne analyseres:  {allLogLines.Where(x => !x.IsValid).Count()}");
+
+            var allStatus = allLogLines.GroupBy(x => x.StatusCode)
+                   .Select(y => y.First())
+                   .ToList();
+
+            foreach (var status in allStatus)
+            {
+                Console.WriteLine($"Antal linier med status {status.StatusCode} ({status.StatusName}) er: {allLogLines.Where(x => x.StatusCode == status.StatusCode).Count()}");
+            }
+
+            Console.WriteLine($"gennemsnittet af millisecods brugt {allLogLines.Where(x => x.IsValid).Select(y => y.MillisecondsSpent).Average()}");
+            Console.WriteLine($"Største og mindste værdi af millisecods spent Størst: {allLogLines.Where(x => x.IsValid).Select(y => y.MillisecondsSpent).Max()} Mindst: {allLogLines.Where(x => x.IsValid).Select(y => y.MillisecondsSpent).Min()}");
+            Console.WriteLine($"mindste balance, der er udestående på en ordre: {allLogLines.Where(x => x.IsValid).Select(y => y.Balance).Min()}");
         }
 
     }
@@ -59,19 +73,47 @@ namespace TivoliApp
     {
         public IList<LogEntry> Load()
         {
-            var allLines = new List<LogEntry>();
+            var allEntries = new List<LogEntry>();
+
+            var allLines = new List<string>();
 
             string line;
 
             StreamReader file = new StreamReader(@"errors.csv");
+            StringBuilder sb = new StringBuilder();
             while ((line = file.ReadLine()) != null)
             {
-                allLines.Add(new LogEntry(line));
+                allLines.Add(line);
+
             }
 
             file.Close();
 
-            return allLines;
+            //Remove header
+            allLines.RemoveAt(0);
+
+            var index = 1;
+
+            foreach (var currentLine in allLines)
+            {
+                sb.Append($"{currentLine} ");
+
+                if (index % 3 == 0)
+                {
+                    allEntries.Add(new LogEntry(sb.ToString()));
+                    sb.Clear();
+                }
+                index++;
+            }
+            // sb.Append(line);
+            // if (line.EndsWith("\""))
+            // {
+            //     allEntries.Add(new LogEntry(sb.ToString()));
+            //     sb.Clear();
+            // }
+
+
+            return allEntries;
         }
     }
 
@@ -80,11 +122,13 @@ namespace TivoliApp
         public string CustomerContactId { get; }
         public string StatusCode { get; }
         public string StatusName { get; }
-        public string details { get; }
-        public string MillisecondsSpent { get; }
+        public string Details { get; }
+        public double MillisecondsSpent { get; }
+
+        public double Balance { get; }
         public string LogLine { get; }
 
-        public bool IsValid{ get; }
+        public bool IsValid { get; }
 
         public LogEntry(string logLine)
         {
@@ -92,13 +136,30 @@ namespace TivoliApp
 
             var lineSplit = logLine.Split(';');
 
-            if (lineSplit?.Length == 4)
+            if (lineSplit?.Length == 5)
             {
                 this.CustomerContactId = lineSplit[0];
                 this.StatusCode = lineSplit[1];
+                this.StatusName = lineSplit[2];
+                this.Details = lineSplit[3];
+
+                Double timeSpent;
+                Double.TryParse(lineSplit[4].Replace("\"", ""), out timeSpent);
+                this.MillisecondsSpent = timeSpent;
+
+                if (this.Details.Contains("with a balance of "))
+                {
+                    var detailsSplit = this.Details.Split("with a balance of ");
+                    Double balance;
+                    Double.TryParse(detailsSplit[1].Split(" ")[0], out balance);
+                    this.Balance = balance;
+                }
+
                 this.IsValid = true;
 
-            } else {
+            }
+            else
+            {
                 this.IsValid = false;
             }
         }

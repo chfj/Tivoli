2440f1da2d0ba2c1f904aece2b87b8c883fc5788
Christian f. Jørgensen <crille@Christians-MacBook-Pro.local>
2020-03-04 10:38:02 +0100

    Created the outline of the program with the data included. And trying out poor mans IoC

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Tivoli
+
+I forbindelse med GDPR skal vi anonymisere udvalgte kontaktinformationer, så man ikke længere kan se deres navn, adresser osv.
+ 
+Til at køre dette, er der skrevet et anonymiseringsprogram, der anonymiserer 100.000 kontakter af gangen. Vi har I alt 3.000.000 kontakter, så anonymiseringsprogrammet skal køres 30 gange. Når anonymiseringsprogrammet kører, vil det generere en logfil over eventuelle fejl fra anonymiseringen.
+ 
+Vedlagt er et eksempel på en logfil, du kan bruge som input.
+ 
+Vi har behov for et C# konsolprogram, der kan indlæse logfilen og besvarer følgende spørgsmål:
+·        Hvor mange rækker er der af hver statuskode
+·        Hvad er gennemsnittet af millisecods spent
+·        Største og mindste værdi af millisecods spent
+·        Hvad er den mindste balance, der er udestående på en ordre
+ 
+Skriv resultaterne ud på konsollen. Størrelsen på logfilerne vil være mellem 0 og 5 mb.
+ 
+Opgaven løses uden NuGet pakker eller andre ”hjælpere” udefra.
+ 
+
+Vi ses på fredag kl. 13 – vi glæder os!
--- TivoliApp/Program.cs
+++ TivoliApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace TivoliApp
 {
@@ -7,6 +9,78 @@ namespace TivoliApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var logViewer = new LogViewer();
+            logViewer.AnalyzeFile();
         }
+    }
+    public class LogViewer
+    {
+        private readonly IFileLineLoader fileLineLoader;
+        public LogViewer() : this(new FileLineLoader())
+        {
+
+        }
+
+        public LogViewer(IFileLineLoader fileLineLoader)
+        {
+            this.fileLineLoader = fileLineLoader;
+        }
+
+        public void AnalyzeFile()
+        {
+            var allLogLines = fileLineLoader.Load();
+
+            var rowsByStatusCode = "";
+
+            
+
+            //Hvor mange rækker er der af hver statuskode
+            //Hvad er gennemsnittet af millisecods spent
+            //Største og mindste værdi af millisecods spent
+            //Hvad er den mindste balance, der er udestående på en ordre
+        }
+
+    }
+
+    public interface IFileLineLoader
+    {
+        public IList<LogEntry> Load();
+    }
+
+    public class FileLineLoader : IFileLineLoader
+    {
+        public IList<LogEntry> Load()
+        {
+            var allLines = new List<LogEntry>();
+            
+            string line;
+  
+            StreamReader file = new StreamReader(@"errors.csv");
+            while ((line = file.ReadLine()) != null)
+            {
+                allLines.Add(new LogEntry(line));
+            }
+
+            file.Close();
+    
+            return allLines;
+        }
+    }
+
+    public class LogEntry
+    {
+        public string CustomerContactId { get; }
+        public string StatusCode { get; }
+        public string StatusName { get; }
+        public string details { get; }
+        public string MillisecondsSpent { get; }
+        public string LogLine { get; }
+
+        public LogEntry(string LogLine)
+        {
+            this.LogLine = LogLine;
+        }
+
     }
 }
